96c9454520a4559edf95df9b7e72a1f38783a9ff
Yan Sá <67102851+Fryhhmil@users.noreply.github.com>
2025-04-13 14:40:14 -0300

    DOCKER

--- Dockerfile
+++ Dockerfile
@@ -1,38 +1,50 @@
+# Stage de build para compilar a aplicação Java
 FROM openjdk:17.0.1-jdk-slim AS java17-to-build
 LABEL authors="fryhh"
 
-RUN apt-get update
-RUN apt-get install -y maven
+# Instalar dependências de build
+RUN apt-get update && apt-get install -y maven
 
-# Copy the app
+# Copiar a aplicação para dentro do container
 WORKDIR /app/
-
 ADD . .
 
+# Limpar pasta de build antiga e compilar o projeto
 RUN rm -fr target
 RUN mvn package -DskipTests
 
 #### FIM DO BUILD #####
 
+# Stage final para rodar a aplicação
 FROM openjdk:17.0.1-jdk-slim
 
+# Configuração de fuso horário
 USER root
 RUN ln -fs /usr/share/zoneinfo/America/Fortaleza /etc/localtime && ls -l /etc/localtime &&\
- echo "America/Fortaleza" > /etc/timezone && cat /etc/timezone
+    echo "America/Fortaleza" > /etc/timezone && cat /etc/timezone
 
-# Copy the app
+# Instalar dependências do Chromium e ChromeDriver
+RUN apt-get update && apt-get install -y \
+    chromium \
+    chromium-driver \
+    gnupg2 \
+    wget \
+    less \
+    && rm -rf /var/lib/apt/lists/*
+
+RUN apt-get update
+
+RUN mkdir -p /home/chrome-profile && chmod 777 /home/chrome-profile
+
+# Definir variáveis de ambiente
+ENV CHROME_BIN="/usr/bin/chromium"
+
+# Expor a porta padrão do Spring Boot
+EXPOSE 8080
+
+# Copiar o JAR gerado para o container
 WORKDIR /app/
 COPY --from=java17-to-build /app/target/*.jar .
 
-## Rodando imagem em um usuário não-root
-RUN addgroup spring && adduser --disabled-password --gecos '' --ingroup spring spring
-USER spring:spring
-
-## ENV
-#ENV SPRING_PROFILES_ACTIVE=prod
-
-## Expõe porta 8080 (padrao de saida do spring)
-EXPOSE 8080
-
-## Executa o primeiro arquivo .jar que encontrar
+# Comando para rodar o JAR com configurações do Spring
 CMD java -Xmx2048m -jar $(ls *.jar | head -n 1) --spring.jpa.show-sql=false --spring.jpa.hibernate.ddl-auto=none
--- src/main/java/br/gov/pi/tce/totvsscraper/service/LoginService.java
+++ src/main/java/br/gov/pi/tce/totvsscraper/service/LoginService.java
@@ -1,6 +1,7 @@
 package br.gov.pi.tce.totvsscraper.service;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
@@ -14,6 +15,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 import java.time.Duration;
 import java.util.Set;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
@@ -21,23 +23,37 @@ import java.util.stream.Collectors;
 public class LoginService {
 
     private WebDriver driver;
-    private WebDriverWait wait;
+    private static final String PORTAL_URL = "https://grupoeducacional127611.rm.cloudtotvs.com.br/FrameHTML/Web/app/edu/PortalEducacional/login/";
+
 
     @PostConstruct
     public void setup() {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new"); // usa o modo headless moderno
-        options.addArguments("--disable-gpu"); // evita alguns bugs gráficos mesmo no headless
-        options.addArguments("--window-size=1920,1080"); // garante uma viewport decente
+        WebDriverManager.chromedriver()
+                .browserVersionDetectionCommand("/usr/bin/chromium --version")
+                .setup();
+    }
 
-        driver = new ChromeDriver(options);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(16));
+    @PreDestroy
+    public void cleanup() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
     public String acessarPortal(String usuario, String senha) {
         try {
-            driver.get("https://grupoeducacional127611.rm.cloudtotvs.com.br/FrameHTML/Web/app/edu/PortalEducacional/login/");
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless=new"); // usa o modo headless moderno
+            options.addArguments("--disable-gpu"); // evita alguns bugs gráficos mesmo no headless
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080"); // garante uma viewport decente
+//            options.addArguments("--user-data-dir=/home/chrome-profile-" + UUID.randomUUID());
+
+            driver = new ChromeDriver(options);
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+            driver.get(PORTAL_URL);
 
             WebElement userField = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("User")));
             WebElement passField = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("Pass")));
@@ -47,15 +63,25 @@ public class LoginService {
             passField.sendKeys(senha);
             acessarBtn.click();
 
-            // Espera o "Carregando" aparecer (opcional, mas ajuda em alguns casos)
-            wait.until(ExpectedConditions.presenceOfElementLocated(
-                    By.xpath("//p[contains(@class, 'text-muted') and contains(text(), 'Carregando')]")
-            ));
+//            // Espera o "Carregando" aparecer (opcional, mas ajuda em alguns casos)
+//            wait.until(ExpectedConditions.presenceOfElementLocated(
+//                    By.xpath("//p[contains(@class, 'text-muted') and contains(text(), 'Carregando')]")
+//            ));
+//
+//            // Espera o "Carregando" desaparecer
+//            wait.until(ExpectedConditions.invisibilityOfElementLocated(
+//                    By.xpath("//p[contains(@class, 'text-muted') and contains(text(), 'Carregando')]")
+//            ));
 
-            // Espera o "Carregando" desaparecer
-            wait.until(ExpectedConditions.invisibilityOfElementLocated(
-                    By.xpath("//p[contains(@class, 'text-muted') and contains(text(), 'Carregando')]")
-            ));
+            wait.until(d -> {
+                String currentUrl = d.getCurrentUrl();
+                System.out.println("URL atual: " + currentUrl);
+                return currentUrl.equals("https://grupoeducacional127611.rm.cloudtotvs.com.br/FrameHTML/Web//app/edu/PortalEducacional/#/");
+            });
+
+
+            wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//p[contains(@class, 'text-muted') and contains(text(), 'Carregando')]")));
+
 
 //            // Vai para a página de faltas
 //            driver.get("https://grupoeducacional127611.rm.cloudtotvs.com.br/FrameHTML/Web//app/edu/PortalEducacional/#/faltas");
@@ -79,6 +105,11 @@ public class LoginService {
         } catch (Exception e) {
             e.printStackTrace();
             return "Erro: " + e.getMessage();
+        }
+        finally {
+            if (driver != null) {
+                driver.quit();
+            }
         }
     }
 }
